8d982b1dd094cc6bfacc212dc60e8ff05a1af5c5
drtamirlan911-cell <dr.tamirlan911@gmail.com>
2026-09-10 09:50:04 +0500

    fix: allow intentional empty catch blocks in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -14,6 +14,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/ban-ts-comment': 'off',
+      'no-empty': ['error', { allowEmptyCatch: true }],
       'no-console': ['warn', { allow: ['error', 'warn'] }],
     },
     languageOptions: {
