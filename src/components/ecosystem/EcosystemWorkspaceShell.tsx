c2088e0ac599045dc4d4a2d7852ee2604da79170
drtamirlan911-cell <dr.tamirlan911@gmail.com>
2026-09-16 21:53:18 +0500

    fix(ecosystem): align workspace shell component exports

--- src/components/ecosystem/EcosystemWorkspaceShell.tsx
+++ src/components/ecosystem/EcosystemWorkspaceShell.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { EcosystemWorkspaceHeader } from './EcosystemWorkspaceHeader';
-import { EcosystemMetrics } from './EcosystemMetrics';
-import { EcosystemQuickActions } from './EcosystemQuickActions';
-import { EcosystemCommandCenter } from './EcosystemCommandCenter';
-import { EcosystemJourneyStrip } from './EcosystemJourneyStrip';
+import EcosystemWorkspaceHeader from './EcosystemWorkspaceHeader';
+import EcosystemMetrics from './EcosystemMetrics';
+import EcosystemQuickActions from './EcosystemQuickActions';
+import EcosystemCommandCenter from './EcosystemCommandCenter';
+import EcosystemJourneyStrip from './EcosystemJourneyStrip';
 import { cn } from '@/lib/utils';
 
 interface Props { className?: string; showCommandCenter?: boolean; showJourneys?: boolean; children?: React.ReactNode }
