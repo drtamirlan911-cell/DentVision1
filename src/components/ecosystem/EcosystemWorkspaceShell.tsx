06d715fd24d6b7a53d77f8d49d09d14faaf212b3
drtamirlan911-cell <dr.tamirlan911@gmail.com>
2026-09-16 21:52:57 +0500

    feat(ecosystem): add composable ecosystem workspace shell

---
+++ src/components/ecosystem/EcosystemWorkspaceShell.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { EcosystemWorkspaceHeader } from './EcosystemWorkspaceHeader';
+import { EcosystemMetrics } from './EcosystemMetrics';
+import { EcosystemQuickActions } from './EcosystemQuickActions';
+import { EcosystemCommandCenter } from './EcosystemCommandCenter';
+import { EcosystemJourneyStrip } from './EcosystemJourneyStrip';
+import { cn } from '@/lib/utils';
+
+interface Props { className?: string; showCommandCenter?: boolean; showJourneys?: boolean; children?: React.ReactNode }
+
+export const EcosystemWorkspaceShell: React.FC<Props> = ({ className, showCommandCenter = true, showJourneys = true, children }) => <div className={cn('space-y-5', className)}>
+  <EcosystemWorkspaceHeader />
+  {showCommandCenter && <EcosystemCommandCenter />}
+  <EcosystemMetrics />
+  <EcosystemQuickActions />
+  {children}
+  {showJourneys && <EcosystemJourneyStrip />}
+</div>;
+
+export default EcosystemWorkspaceShell;
